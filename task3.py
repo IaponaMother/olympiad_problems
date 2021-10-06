1ef14beaf06ad51731f274f19aa79896929f07d2
IaponaMother <58094880+IaponaMother@users.noreply.github.com>
2021-10-06 19:00:36 +0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,3 +1,5 @@
+import time
+start = time.time()
 N = int(input())
 
 P = [1]
@@ -8,8 +10,11 @@ for num in range(2, N + 1):
             prime = False
     if prime:
        P.append(num)
+
 r = 0
 for i in P:
     if N % i == 0:
         r += P.count(N // i)
+
 print(r)
+print(time.time() - start)
