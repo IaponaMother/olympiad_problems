178f4d906e0431521deb4dbf7bb64accf12bf0ae
IaponaMother <58094880+IaponaMother@users.noreply.github.com>
2022-10-21 23:55:17 +0700

    Update domashka.py

--- domashka.py
+++ domashka.py
@@ -8,4 +8,3 @@ for i in range(len(s)):
     # r += alf[(alf.index(s[i]) + alf.index(f[i]) + 1) % 32], я хз зачем здесь + 1, хрень, а не задачник
     # r += alf[alf.index(s[i]) - alf.index(f[i]) - 1], 18 задание со всеми буквами алфавита
 print(r)
-print(len(alf))
