a20d9de9d04527e137cb6ca1680f13e95f23a700
IaponaMother <58094880+IaponaMother@users.noreply.github.com>
2022-11-12 16:36:16 +0700

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -28,3 +28,33 @@ int main() {
     std::cout<<r2<<std::endl;
     return 0;
 }
+
+
+// #include <iostream>
+// using namespace std;
+
+// void print_array(int** ar, int m, int n) {
+//     for (int i = 0; i < m; ++i) {
+//         for (int j = 0; j < n; ++j) {
+//             cout << ar[i][j]<<" ";
+//         }
+//         cout << endl;
+//     }
+// }
+// int main()
+// {
+//     int size;
+//     cout << "enter the size of the array: ";
+//     cin >> size;
+//     int** array = new int* [size];
+//     for (int i = 0; i < size; ++i) {
+//         array[i] = new int(size);
+//         for (int j = 0; j < size; ++j) {
+//             cout << "enter array element: ";
+//             cin >> array[i][j];
+//         }
+//     }
+//     print_array(array, size, size);
+//     return 0;
+    
+// }
