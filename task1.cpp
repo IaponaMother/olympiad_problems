a6fd801970ed10958a6cb55c5a6da79054fffdc7
IaponaMother <58094880+IaponaMother@users.noreply.github.com>
2022-11-15 19:41:26 +0700

    Create task1.cpp

---
+++ task1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int* a = new int[2];
+    for (int i = 0; i < 2; i++){
+        cin>>a[i];
+    }
+    
+    if (a[1] - a[0] == 1) cout<<a[0];
+    else{
+        if(a[1] <= a[0]){
+            if (a[0] % a[1] == 0) cout<<0;
+            else cout<<a[0] * a[1];
+            
+        }
+        else{
+            if (a[1] % a[0] == 0) cout<<(a[1] / a[0] - 1) * a[0];
+            else cout<<a[0] * a[1];
+        }
+            
+        }
+        
+    }
