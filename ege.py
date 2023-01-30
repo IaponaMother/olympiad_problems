f548f63c9da2cd08402b9f3081a9a57f6acd5e29
IaponaMother <58094880+IaponaMother@users.noreply.github.com>
2023-01-30 13:30:26 +0700

    Create ege.py

---
+++ ege.py
@@ -0,0 +1,39 @@
+# 3 задание
+# r = 0
+# for x1 in range(2):
+#     for x2 in range(2):
+#         for x3 in range(2):
+#             for x4 in range(2):
+#                 for x5 in range(2):
+#                     for x6 in range(2):
+#                         for x7 in range(2):
+#                             for x8 in range(2):
+#                                 for x9 in range(2):
+#                                     if x1 == x2 or ((not x3 and x1) or (not x1 and x3)) == 0:
+#                                         if x2 == x3 or ((not x4 and x2) or (not x2 and x4)) == 0:
+#                                             if x3 == x4 or ((not x5 and x3) or (not x3 and x5)) == 0:
+#                                                 if x4 == x5 or ((not x6 and x4) or (not x4 and x6)) == 0:
+#                                                     if x5 == x6 or ((not x7 and x5) or (not x5 and x7)) == 0:
+#                                                         if x6 == x7 or ((not x8 and x6) or (not x6 and x8)) == 0:
+#                                                             if x7 == x8 or ((not x9 and x7) or (not x7 and x9)) == 0:
+#                                                                 r += 1
+# print(r)
+
+# 2 задание 
+# r = 0
+# for x1 in range(2):
+#     for x2 in range(2):
+#         for x3 in range(2):
+#             for x4 in range(2):
+#                 for x5 in range(2):
+#                     for x6 in range(2):
+#                         for x7 in range(2):
+#                             for x8 in range(2):
+#                                 if x1 == x2 or ((not x3 and x1) or (not x1 and x3)) == 0:
+#                                     if x2 == x3 or ((not x4 and x2) or (not x2 and x4)) == 0:
+#                                         if x3 == x4 or ((not x5 and x3) or (not x3 and x5)) == 0:
+#                                             if x4 == x5 or ((not x6 and x4) or (not x4 and x6)) == 0:
+#                                                 if x5 == x6 or ((not x7 and x5) or (not x5 and x7)) == 0:
+#                                                     if x6 == x7 or ((not x8 and x6) or (not x6 and x8)) == 0:
+#                                                         r += 1
+# print(r)
